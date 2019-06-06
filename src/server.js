a704a94309c3828db17a8a9b845b4310f2bd3957
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-06-06 06:52:31 -0700

    added mongoDB

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-// require("dotenv").config();
-// const bodyParser = require("body-parser");
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
-// app.use(bodyParser.json());
+const mongoose = require("mongoose", () => console.log("moongose connected"));
+
+app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
+console.log("db", process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }, () =>
+  console.log("mongose connected")
+);
+
 app.set("port", process.env.PORT || 3001);
 app.listen(app.get("port"), () => {
   console.log(`Server at: http://localhost:${app.get("port")}/`);
