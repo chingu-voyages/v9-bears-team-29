a841750937ab3feb1c7939c6eecfb92515bdd9d5
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-06-06 06:57:43 -0700

    removed deprecrated param

--- src/server.js
+++ src/server.js
@@ -8,9 +8,7 @@ const mongoose = require("mongoose", () => console.log("moongose connected"));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 console.log("db", process.env.DB_URI);
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }, () =>
-  console.log("mongose connected")
-);
+mongoose.connect(process.env.DB_URI, () => console.log("mongose connected"));
 
 app.set("port", process.env.PORT || 3001);
 app.listen(app.get("port"), () => {
