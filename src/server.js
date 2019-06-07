c37b7093c0a5ab4077273af345553fb526287038
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-06-07 15:56:49 -0700

    deleted console.log

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const mongoose = require("mongoose", () => console.log("moongose connected"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log("db", process.env.DB_URI);
 mongoose.connect(process.env.DB_URI, () => console.log("mongose connected"));
 
 app.set("port", process.env.PORT || 3001);
