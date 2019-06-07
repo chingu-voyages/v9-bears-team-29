b29009f8dd1a62ad998a6e65cc08fcb89c9b30f6
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-06-07 15:53:58 -0700

    resolved merging commits

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { history } from '../helpers/history';
-import { alertActions } from '../actions/alertActions';
-import { Router, Switch, Route } from 'react-router-dom';
-import Landing from './Landing';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { history } from "../helpers/history";
+import { alertActions } from "../actions/alertActions";
+import { Router, Switch, Route } from "react-router-dom";
+import Landing from "./Landing";
 
 class ConnectedApp extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { dispatch } = this.props;
-        history.listen((location, action) => {
-            dispatch(alertActions.clear())
-        })
-    }
+    const { dispatch } = this.props;
+    history.listen((location, action) => {
+      dispatch(alertActions.clear());
+    });
+  }
 
-    render() {
-        return(
-            <div>
-                {alert.message && <div className={`alert ${alert.type}`}>{alert.message}</div>}
-                <Router history={history}>
-                <div className="App">
-                <main className="content">
-                    <Switch>
-                        <Route exact path="/" component={Landing}/>
-                    </Switch>
-                </main>
-                </div>
-            </Router>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {alert.message && (
+          <div className={`alert ${alert.type}`}>{alert.message}</div>
+        )}
+        <Router history={history}>
+          <div className="App">
+            <main className="content">
+              <Switch>
+                <Route exact path="/" component={Landing} />
+              </Switch>
+            </main>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    const { alert } = state;
-    return {
-        alert
-    };
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+    alert
+  };
 }
 
 const App = connect(mapStateToProps)(ConnectedApp);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import { alert } from './alertReducer';
+import { combineReducers } from "redux";
+import { alert } from "./alertReducer";
 //import all reducers here
 
 const rootReducer = combineReducers({
-//add all new reducers here
-    alert,
-})
+  //add all new reducers here
+  alert
+});
 
 export default rootReducer;
